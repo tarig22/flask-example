cb80f22620b7b5030016d67184904bee78116761
tarig22 <55983151+tarig22@users.noreply.github.com>
2021-09-02 17:57:07 +0200

    Update hello.py

--- app/hello.py
+++ app/hello.py
@@ -12,10 +12,10 @@ def getData():
 
 
 app = Flask(__name__)
-CORS(app, support_credentials=True)
+CORS(app)
 
 @app.route("/" , methods=['POST', 'GET','OPTIONS'])
-@cross_origin(supports_credentials=True)
+@cross_origin()
 def index():
     return getData() , 200, {'Content-Type': 'text/html; charset=UTF-8'}
 #     return render_template("index.html", data=getData())
